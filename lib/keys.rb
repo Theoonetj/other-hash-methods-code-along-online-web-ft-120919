f695ff23142c924a075aea1312b53c3b5a2d82fc
confident-token-1019 <theoonetj@gmail.com>
2019-12-20 21:35:01 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -4,10 +4,11 @@ require 'pry'
  groceries = {
  dairy: ["milk", "yogurt", "cheese"],
  vegetable: ["carrots", "broccoli", "cucumbers"],
-#  meat: ["chicken", "steak", "salmon"],
-#  grains: ["rice", "pasta"]
-# }
+ meat: ["chicken", "steak", "salmon"],
+  grains: ["rice", "pasta"]
+ }
 
 def get_the_keys(groceries)
   #code your solution here!
+  groceries.keys
 end
